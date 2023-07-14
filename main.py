49608a718d5e2d873f3aee8dd3e7b258a13e9c1d
Bartek <bartex600700@gmail.com>
2023-07-14 23:52:56 +0200

    commit

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 # Password Generator Project
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-password_list = []
-[password_list.append(choice(letters)) for _ in range(randint(8, 10))]
-[password_list.append(choice(symbols)) for _ in range(randint(2, 4))]
-[password_list.append(choice(numbers)) for _ in range(randint(2, 4))]
+    password_list = []
+    [password_list.append(choice(letters)) for _ in range(randint(8, 10))]
+    [password_list.append(choice(symbols)) for _ in range(randint(2, 4))]
+    [password_list.append(choice(numbers)) for _ in range(randint(2, 4))]
 
-shuffle(password_list)
-password = "".join(password_list)
-
-print(f"Your password is: {password}")
-
+    shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_data():
@@ -66,13 +68,13 @@ website_entry.focus()
 
 email_entry = Entry(width=50)
 email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0, "name@mail.com")
+email_entry.insert(0, "bartek@gmail.com")
 
 password_entry = Entry(width=32)
 password_entry.grid(column=1, row=3)
 
 # Buttons
-generate_button = Button(text="Generate Password", width=14)
+generate_button = Button(text="Generate Password", width=14, command=generate_password)
 generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=43, command=save_data)
